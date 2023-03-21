fe78cc8a31009c85b30456a719c03f16039a7b77
Chen <1634691255@qq.com>
2023-03-21 14:03:56 +0800

    first

---
+++ src/main/java/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        //1111111
+
+        System.out.println("22222");
+    }
+}
+/*
+[core]
+excludesfile = F:/Git/git.ignore
+ */
